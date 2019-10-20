93cb874b4f96559a837691cbced0ba3b39d5a414
mariomui <yi.kan.mui@gmail.com>
2019-10-19 18:01:05 -0700

    Added basic server-side logic

--- gameEngine/Game.js
+++ gameEngine/Game.js
@@ -1,15 +1,27 @@
 const { generateRandomNumbers } = require('../serverUtils');
+
 class Game {
   constructor(Player, playerSettings) {
     this.player = Player;
     this.playerSettings = playerSettings
     this.currentStage = 6
     this.hasGameStarted = false;
-    this.secret
+    this.secretWord = null;
+    this.currentGuess = [];
   }
 
   generateSecretWord() {
     let gameSettings = this.player.getGameSettings()
 
   }
+
+  startGame() {
+
+  }
+
+  endGame() {
+
+  }
+
+
 }
--- gameEngine/Player.js
+++ gameEngine/Player.js
@@ -3,6 +3,12 @@ class Player {
     this.name = name;
     this._gameSettings = null;
   }
+  /**
+   * 
+   * @param {*} gameSettings 
+   * difficulty: 2, 4, 8
+   * 
+   */
   createGameSettings(gameSettings) {
     this._gameSettings = gameSettings;
     return !!this._gameSettings
